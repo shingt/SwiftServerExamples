dfd3f9fea0b9c619556fb966c03047d4687db730
shingt <van.s.ver@gmail.com>
2015-10-04 18:22:46 +0900

    Add server using Swifter

---
+++ swifterServer.swift
@@ -0,0 +1,39 @@
+import Swifter
+import Mustache
+
+func templatePath(path: String) -> String {
+    return "./views/" + path + ".mustache"
+}
+
+let server = HttpServer()
+
+    server["/"] = { request in
+
+        let documentName: String = "document"
+        let path = templatePath("top")
+        let template = try! Template(path: path)
+
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = .MediumStyle
+        template.registerInBaseContext("format", Box(dateFormatter))
+
+        let data = [
+        "name": "shibuya.swift",
+        "date": NSDate(),
+        ]
+
+        let rendering: String = try! template.render(Box(data))
+
+        return .OK(.HTML(rendering))
+    }
+
+var error: NSError?
+
+let port: UInt16 = 3002
+if !server.start(port, error: &error) {
+    print("Server start error: \(error)")
+} else {
+    print("Server started on port: \(port)")
+    NSRunLoop.mainRunLoop().run()
+}
+
