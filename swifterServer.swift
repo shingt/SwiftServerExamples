4ca69835c4d3c920f0b20b222d511f87fe916c5f
shingt <van.s.ver@gmail.com>
2015-10-15 00:01:57 +0900

    Fix entry id

--- swifterServer.swift
+++ swifterServer.swift
@@ -178,10 +178,10 @@ server["/diary/entry/(.+)"] = { request in
     do {
         let template = try Template(path: path)
 
-        let entry_id: Int? = (request.capturedUrlGroups.last as String)!.toInt()
+        let entry_id: Int? = Int(request.capturedUrlGroups.last as String!)
         let account_name = "someone"
         let owner = userFromAccount(account_name)
-        let entry = getEntry(entry_id)
+        let entry = getEntry(entry_id!)
 
         let data = [
             "owner": owner,
